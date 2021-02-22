ba0f25c4af4bcb52f5a303887abd7b21671698d0
Sergey Fedoseenkov <frodor67@bk.ru>
2021-02-22 23:26:40 +0300

    feat: новый проект, настроить таймер

---
+++ script/script.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    // Timer
+    const countTimer = deadline => {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        const getTimerRemaining = () => {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+                //day = Math.floor(timeRemaining / 60 / 60 / 24);
+            return { timeRemaining, hours, minutes, seconds };
+        };
+
+        const timerId = setInterval(() => {
+            const timer = getTimerRemaining();
+
+            if (String(timer.hours).length === 1) {
+                timerHours.textContent = ('0' + timer.hours);
+            } else {
+                timerHours.textContent = timer.hours;
+            }
+
+            if (String(timer.minutes).length === 1) {
+                timerMinutes.textContent = ('0' + timer.minutes);
+            } else {
+                timerMinutes.textContent = timer.minutes;
+            }
+
+            if (String(timer.seconds).length === 1) {
+                timerSeconds.textContent = ('0' + timer.seconds);
+            } else {
+                timerSeconds.textContent = timer.seconds;
+            }
+
+            if (timer.timeRemaining <= 0) {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                clearInterval(timerId);
+            }
+        }, 1000);
+
+    };
+    countTimer('22 february 2021');
+
+});
