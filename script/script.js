a8642ed01ce5dc0fcc645739dc719e06b438d243
Sergey Fedoseenkov <frodor67@bk.ru>
2021-03-03 23:07:17 +0300

    feat: дописать калькултор

--- script/script.js
+++ script/script.js
@@ -281,8 +281,61 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //калькулятор
 
-    const calc = () => {
-        const calcItem = document.querySelectorAll('.calc-item');
+    const calc = (price = 100) => {
+        const calcItem = document.querySelectorAll('.calc-item'),
+            calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.querySelector('#total');
+
+        const countSum = () => {
+            let total = 20,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            } else {
+                total = 0;
+            }
+
+            
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', e => {
+            const target = e.target;
+
+            // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            // target.matches('.calc-day') || target.matches('.calc-count')) {
+
+            // }
+
+            // if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+
+            // }
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+
+        });
+
+
 
         calcItem.forEach(item => {
             item.addEventListener('input', () => {
